5c9d22e80b716d3f40892d1a09f515690f57559a
Chanhtin13 <tin0373019800@gmail.com>
2024-09-24 20:40:25 +0700

    bt7/73-74

--- ConsoleApp1/obj/Debug/net6.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net6.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3e9b35a891fea0442105a5300a3a5bbf7021cb3")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ ConsoleApp1/trang73_74.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class trang73_74
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("nhap so giay:");
+            int giay = Convert.ToInt32(Console.ReadLine());
+            int phut = (giay % 3600) / 60;
+            int gio = giay / 3600;
+            int remainingSeconds = giay % 60;
+            Console.WriteLine($"{gio}:{phut}:{remainingSeconds}");
+        }
+    }
+}
